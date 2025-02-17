892cc2321914ca8f75445c94a27d9474242b4a9a
Saanicc <mattias470@gmail.com>
2025-02-17 19:28:25 +0100

    Update text

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -9,9 +9,9 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
-    title: "Mobile wallpaper gallery app",
+    title: "Wallpaper gallery app",
     description:
-      "A cross-platform wallpaper gallery app that allows you to browse wallpapers by category and sort by popularity or latest upload on your device and get information about each wallpaper. There is also a button on each wallpaper that allows you to navigate to the uploader's profile on Pixabay or Pixabays download page. The app is built using Expo React Native and uses the Pixabay API to fetch wallpapers.",
+      "A cross-platform wallpaper gallery app that allows you to browse wallpapers by category and sort by popularity or latest upload on your device and get information about each wallpaper. There is also a button on each wallpaper that allows you to navigate to the uploader's profile on Pixabay or Pixabays download page. The app is built using Expo React Native and uses the Pixabay API to fetch wallpapers. The data you fetch from the API is also cached in your devices local storage for 24 hours.",
     technologies: [
       "React Native",
       "Expo",
@@ -20,26 +20,18 @@ export const projects: Project[] = [
       "Tanstack Query",
       "React Native Reanimated",
     ],
-    github: "https://github.com/Saanicc/wallpaper-gallery",
+    github: "https://github.com/Saanicc/WallpaperGallery",
   },
   {
     id: 2,
     title: "Project 2",
     description: "Description of Project 2",
+    technologies: ["Lorem", "Ipsum", "Dolor"],
   },
   {
     id: 3,
     title: "Project 3",
     description: "Description of Project 3",
-  },
-  {
-    id: 4,
-    title: "Project 4",
-    description: "Description of Project 4",
-  },
-  {
-    id: 5,
-    title: "Project 5",
-    description: "Description of Project 5",
+    technologies: ["Lorem", "Ipsum", "Dolor"],
   },
 ];
