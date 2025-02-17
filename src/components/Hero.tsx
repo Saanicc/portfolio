2691793fa2b0c70a228813b04dec31dfa01f34fa
Saanicc <mattias470@gmail.com>
2025-02-17 15:40:42 +0100

    Update hero style and add scroll down button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ interface HeroProps {
 
 export default function Hero({ scrollToProjects }: HeroProps) {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold text-white text-wrap max-w-2xl mx-auto text-center">
         <span className="inline-block bg-gradient-to-r from-[#63e] via-gray-100 to-[#63e] text-transparent bg-clip-text bg-[length:200%_100%] animate-wave-text">
           Mattias Ahlström
@@ -15,19 +15,20 @@ export default function Hero({ scrollToProjects }: HeroProps) {
         <br />
         Frontend Developer
       </h1>
-      <p className="text-lg text-white mt-4 text-center max-w-2xl">
+      <p className="text-lg text-white mt-6 text-center max-w-2xl">
         I specialize in creating seamless user experiences for web and mobile
         applications using React and React Native. Passionate about building
         intuitive, responsive interfaces that users love.
       </p>
       <Button
         variant="ghost"
-        size="icon"
         onClick={scrollToProjects}
-        className="mt-12 animate-bounce hover:bg-white/10"
+        className="mt-12 hover:bg-transparent flex flex-col items-center justify-center gap-0 h-fit"
       >
-        <ChevronDown className="h-8 w-8 text-white" />
+        <ChevronDown className="min-h-4 min-w-4 text-white animate-bounce" />
+        <ChevronDown className="min-h-5 min-w-5 text-white animate-bounce [animation-delay:150ms]" />
+        <ChevronDown className="min-h-6 min-w-6 text-white animate-bounce [animation-delay:300ms]" />
       </Button>
-    </>
+    </div>
   );
 }
