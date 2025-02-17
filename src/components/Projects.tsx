2c8cf7485aa0c33cf6d1a1c07155d23588e6f032
Saanicc <mattias470@gmail.com>
2025-02-17 19:29:05 +0100

    Create ProjectCard component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,5 @@
 import ProjectCard from "./ProjectCard";
+import { projects } from "@/lib/data/projects";
 
 export default function Projects() {
   return (
@@ -6,11 +7,11 @@ export default function Projects() {
       id="projects"
       className="min-h-screen flex flex-col items-center justify-center"
     >
-      <h2 className="text-3xl font-bold text-white mb-12">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl px-4">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <h2 className="text-3xl font-bold text-white mb-12">My projects</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl px-4">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
       </div>
     </section>
   );
