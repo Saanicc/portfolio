a0de6283c40c86c782fac1c30d38c7489472e6b5
Saanicc <mattias470@gmail.com>
2025-02-18 19:20:02 +0100

    Animate Hero content using gsap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,50 @@
 import { Button } from "@/components/ui/button";
+import { useGSAP } from "@gsap/react";
 import { ChevronDown } from "lucide-react";
+import { gsap } from "gsap";
 
 interface HeroProps {
   scrollToAbout: () => void;
 }
+gsap.registerPlugin(useGSAP);
 
 export default function Hero({ scrollToAbout }: HeroProps) {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".header",
+      { opacity: 0, x: -500 },
+      { opacity: 1, x: 0, duration: 1.5, ease: "power1.inOut" }
+    );
+
+    gsap.fromTo(
+      ".info-text",
+      { opacity: 0, x: 500 },
+      { opacity: 1, x: 0, duration: 1.5, ease: "power1.inOut" }
+    );
+
+    gsap.fromTo(
+      ".chevron",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1.25,
+        duration: 0.5,
+        ease: "power1.inOut",
+      }
+    );
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-white text-wrap max-w-2xl mx-auto text-center">
+      <h1 className="header text-4xl font-bold text-white text-wrap max-w-2xl mx-auto text-center">
         <span className="inline-block bg-gradient-to-r from-[#63e] via-gray-100 to-[#63e] text-transparent bg-clip-text bg-[length:200%_100%] animate-wave-text">
           Mattias Ahlström
         </span>
         <br />
         Frontend Developer
       </h1>
-      <p className="text-lg text-white mt-6 text-center max-w-2xl">
+      <p className="info-text text-lg text-white mt-6 text-center max-w-2xl">
         I specialize in creating seamless user experiences for web and mobile
         applications using React and React Native. Passionate about building
         intuitive, responsive interfaces that users love.
@@ -23,11 +52,11 @@ export default function Hero({ scrollToAbout }: HeroProps) {
       <Button
         variant="ghost"
         onClick={scrollToAbout}
-        className="mt-12 hover:bg-transparent flex flex-col items-center justify-center gap-0 h-fit"
+        className="chevron mt-12 hover:bg-transparent flex flex-col items-center justify-center gap-0 h-fit"
       >
-        <ChevronDown className="min-h-4 min-w-4 text-white animate-bounce" />
-        <ChevronDown className="min-h-5 min-w-5 text-white animate-bounce [animation-delay:150ms]" />
-        <ChevronDown className="min-h-6 min-w-6 text-white animate-bounce [animation-delay:300ms]" />
+        <ChevronDown className="min-h-4 min-w-4 text-white animate-bounce [animation-delay:1000ms]" />
+        <ChevronDown className="min-h-5 min-w-5 text-white animate-bounce [animation-delay:1150ms]" />
+        <ChevronDown className="min-h-6 min-w-6 text-white animate-bounce [animation-delay:1300ms]" />
       </Button>
     </div>
   );
