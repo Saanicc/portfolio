4e821b02425db17ba04392a364ad2ce8f2093236
Saanicc <mattias470@gmail.com>
2026-02-03 23:15:36 +0100

    Add Animate UI's StarsBackground component

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -1,5 +1,17 @@
+import { cn } from "@/lib/utils";
+import { StarsBackground } from "./animate-ui/components/backgrounds/stars";
+
 export const BackgroundGradient = () => {
   return (
-    <div className="fixed min-h-dvh inset-0 -z-10 w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,hsl(var(--accent))_100%)]"></div>
+    <div className="fixed min-h-dvh -z-10 w-full h-full">
+      <StarsBackground
+        speed={150}
+        starColor="#FFFFFF80"
+        className={cn(
+          "absolute inset-0 flex items-center justify-center rounded-xl",
+          "bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,hsl(var(--accent))_100%)]",
+        )}
+      />
+    </div>
   );
 };
